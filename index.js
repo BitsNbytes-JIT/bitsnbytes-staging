26ff622179fc1954aff021718104930805109de5
ruthvikl <48211227+ruthvikl@users.noreply.github.com>
2021-05-06 12:47:54 +0530

    intialized project

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, ()=> { console.log(`Server is running on http://localhost:${port}`)});
