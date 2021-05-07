8491f095b60976fda75ef0dbaf2b163b3dc61585
ruthvikl <48211227+ruthvikl@users.noreply.github.com>
2021-05-07 16:11:39 +0530

    experiments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Index from './views/Index';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" render={(props) => <Index {...props} />} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
