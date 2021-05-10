3e3e50c33e670503a7c516ac2a0f1407385028a3
ruthvik <ruthvikbhattl@gmail.com>
2021-05-10 18:32:57 +0530

    Added logo and favicon

--- frontend/src/views/Index.js
+++ frontend/src/views/Index.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bits N Bytes
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://forms.gle/dn1KaKcrtewguxj57"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Register here
         </a>
       </header>
       <Footer/>
