3aeb8dbf0f218a394bb6e8de4077e51feb982e14
ruthvik <ruthvikbhattl@gmail.com>
2021-06-02 11:32:02 +0530

    updated

--- routes/api/users.js
+++ routes/api/users.js
@@ -85,7 +85,7 @@ router.post("/login", (req, res) => {
         // Sign token
         jwt.sign(
           payload,
-          process.env.secretOrKey,
+          process.env.SECRET,
           {
             expiresIn: 31556926 // 1 year in seconds
           },
